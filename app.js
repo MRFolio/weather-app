d468a872f315acf18b9d4fe202c5d61e27a54ff8
MRFolio <roose.markus@gmail.com>
2020-10-11 23:29:42 +0300

    feat(weather-app): add fetch and display data

--- app.js
+++ app.js
@@ -10,33 +10,28 @@ const getForecast = async (city) => {
   try {
     const response = await fetch(urlToFetch);
     const data = await response.json();
-    const { name } = data;
-    const { temp } = data.main;
-    return { name, temp };
+    console.log(data);
+    const { name, main: { temp }, wind: { speed }, weather: [{ icon, main }] } = data
+
+    /* const { name, main: { temp }, wind: { speed } } = data;
+    const { icon, main } = data.weather[0]; */
+    return { name, temp, speed, icon, main };
   } catch (error) {
     console.log(error);
   }
 };
 
-const displayData = async () => {
-  /* const fetchData = getForecast(userInput.value); */
-  const fetchData = await getForecast("Tartu");
-
-  console.log(fetchData);
-  /* const result = `<h4 class="city-name">${name}</h4>
-<div class="city-icon"></div>
-<div class="content">Temperature is ${main.temp} </div>`;
-  container.innerHTML = result */
+const displayData = async (city) => {
+  const { name, temp, speed, icon, main } = await getForecast(city);
+  const result = `<h4 class="city-name">${name}</h4>
+  <div class="content"><p>Temperature is ${temp.toFixed(1)} &#8451</p></div>
+  <div class="city-icon"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather-icon"><p>Condition: ${main}</p><p>Wind Speed: ${speed} m/s</p></div>
+  `;
+  container.innerHTML = result;
 };
 
-displayData();
-
-/* const tere = (tere2) => {
-  const tere3 = getForecast("Tartu");
-  console.log(tere3);
-};
-tere(); */
-/* userInput.addEventListener("submit", (e) => {
+userInput.addEventListener("input", (e) => {
   e.preventDefault();
-  displayData();
-}); */
+  const inputCity = e.target.value.trim();
+  displayData(inputCity);
+});
