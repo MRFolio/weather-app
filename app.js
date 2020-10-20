716db181fa71444faf6d5c19227f5e30785c728a
MRFolio <roose.markus@gmail.com>
2020-10-20 04:37:56 +0300

    feat(weather-app): add almost completed project

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import key from "./key.js";
 
 const container = document.querySelector(".result");
 const userInput = document.getElementById("city");
+const bodyClass = document.querySelector("body");
 
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
 
@@ -11,51 +12,56 @@ const getForecast = async (city) => {
     const response = await fetch(urlToFetch);
     const data = await response.json();
     console.log(data);
-    const { name, main: { temp }, wind: { speed }, weather: [{ icon, main }] } = data
-    return { name, temp, speed, icon, main };
+    const {
+      name,
+      main: { temp },
+      sys: { country },
+      weather: [{ icon, main }],
+    } = data;
+    return { name, temp, country, icon, main };
   } catch (error) {
     console.log(error);
   }
 };
 
-const displayData = async (city) => {
-  const { name, temp, speed, icon, main } = await getForecast(city);
- /*  const date = new Date();
+const getDate = () => {
+  const date = new Date();
+  const result = date.toLocaleTimeString([], { timeStyle: "short" });
   const string = date.toString();
-  const dateArray = string.split(" ")
-  const dateDisplayText = dateArray.slice(0,5)
-  console.log(dateDisplayText); */
-  const result = `<h4 class="city-name">${name}</h4>
-  <div class="content"><p>Temperature is ${temp.toFixed(1)} &#8451</p></div>
-  <div class="city-icon"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather-icon"><p>Condition: ${main}</p><p>Wind Speed: ${speed} m/s</p></div>
-  `;
-  container.innerHTML = result;
-  getDate();
+  const dateArray = string.split(" ");
+  const dateDisplayText = dateArray.slice(0, 5);
+  const res = `<div class="date-time"><span class="date">${dateDisplayText[0]} ${dateDisplayText[1]} ${dateDisplayText[2]}</span><span class="time">, ${result}</span></div>`;
+  container.innerHTML = res;
 };
 
-userInput.addEventListener("input", (e) => {
+const backgroundColor = (temp) => {
+  if (temp < 15) {
+    bodyClass.classList.add("cold");
+    container.classList.add("cold");
+  } else {
+    container.classList.remove("cold");
+    bodyClass.classList.remove("cold");
+  }
+};
+
+const displayData = async (city) => {
+  const { name, temp, country, icon, main } = await getForecast(city);
+  getDate();
+  const result = `<div class="content">
+  <h4 class="city-name">${name}, ${country}</h4><p class="temp">${temp.toFixed(
+    1
+  )} <span class="degree">&#8451</span></p></div>
+  <div class="city-icon"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather-icon"><span class="description">${main}</span></div>
+  `;
+  container.innerHTML += result;
+  container.classList.add("show");
+  backgroundColor(temp);
+};
+
+function showCity(e) {
   e.preventDefault();
   const inputCity = e.target.value.trim();
   displayData(inputCity);
-});
-
-
-
-const getDate = () => {
-  const date = new Date();
-  const result = date.toLocaleTimeString([], {timeStyle: 'short'});
-  /* const result2 = date.toLocaleDateString()
-  const result3 = date.toLocaleString()
-  console.log(result3);
-  console.log(result2);
-  console.log(result);const now = date.getDate();
-  console.log(now);
-  console.log(date);
-  const string = date.toString();
-  const dateArray = string.split(" ")
-  const dateDisplayText = dateArray.slice(0,5) */
-  container.innerHTML += date;
-  console.log(dateDisplayText);
-
 }
 
+userInput.addEventListener("input", showCity);
